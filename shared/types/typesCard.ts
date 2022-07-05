0478c3e2f4923c85a52c21051dce28bf73fc8af0
Oskar <oskar.baumann@tum.de>
2022-07-05 21:50:41 +0200

    Change shared linting

--- shared/types/typesCard.ts
+++ shared/types/typesCard.ts
@@ -10,7 +10,7 @@ export interface cardsType {
     meisterVersion: boolean
 }
 
-export type cardType = string//"1"|"2"|"3"|"4"|"5"|"6"|"7"|"7-0"|"7-1"|"7-2"|"7-3"|"7-4"|"7-5"|"7-6"|"8"|"9"|"10"|"12"|"13"|"tac"|"tac-0"|"tac-1"|"tac-2"|"tac-3"|"tac-4"|"tac-5"|"tac-6"|"trickser"|"narr"|"teufel"|"engel"|"kriger"
+export type cardType = string
 
 export interface playerCard {
     possible: boolean,
