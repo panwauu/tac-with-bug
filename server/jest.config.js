2c15b5425b3334cd29b87536db49c24b5f5952ec
Oskar <oskar.baumann@tum.de>
2022-07-10 17:07:49 +0200

    Add login test and remove from jest config

--- server/jest.config.js
+++ server/jest.config.js
@@ -3,6 +3,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   coveragePathIgnorePatterns: ['./src/test'],
-  modulePathIgnorePatterns: ['./dist'],
+  modulePathIgnorePatterns: ['./dist', './src/tests'],
   setupFilesAfterEnv: ['./src/test/setupTestEnvironment.ts'],
 }
