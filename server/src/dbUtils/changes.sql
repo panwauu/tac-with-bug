1be6223d2e06f58372741c658b9bf9c3062f78ea
Oskar <oskar.baumann@tum.de>
2022-07-31 23:32:06 +0200

    Apply to database

--- server/src/dbUtils/changes.sql
+++ server/src/dbUtils/changes.sql
@@ -1,22 +1,2 @@
 ALTER TABLE
-    games
-ADD
-    COLUMN running BOOLEAN NOT NULL DEFAULT TRUE;
-
-UPDATE
-    games
-SET
-    running = FALSE
-WHERE
-    status != 'running';
-
-ALTER TABLE
-    users_to_games DROP CONSTRAINT users_to_games_player_index_check;
-
-ALTER TABLE
-    users_to_games
-ADD
-    CONSTRAINT users_to_games_player_index_check CHECK (player_index >= 0);
-
-ALTER TABLE
     games DROP COLUMN status;
