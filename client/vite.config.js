66cd3e3bf3c33937b0bd22f0230d298d5506a675
Oskar <oskar.baumann@tum.de>
2022-10-11 18:16:00 +0200

    Update vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import eslintPlugin from 'vite-plugin-eslint'
+import eslint from 'vite-plugin-eslint'
 import Checker from 'vite-plugin-checker'
 import path from 'path'
 import loadVersion from 'vite-plugin-package-version'
 
 export default defineConfig({
-  plugins: [vue(), eslintPlugin({ throwOnError: false, throwOnWarning: false }), Checker({ vueTsc: true }), loadVersion()],
+  plugins: [vue(), eslint({ failOnError: false, failOnWarning: false }), Checker({ vueTsc: true }), loadVersion()],
   server: {
     port: 8080,
     proxy: {
