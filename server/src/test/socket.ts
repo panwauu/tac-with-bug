7e226865946721655ffa7b3e5d8d500db715b62e
Oskar <oskar.baumann@baumann-automation.com>
2023-05-22 16:47:47 -0600

    Update server deps

--- server/src/test/socket.ts
+++ server/src/test/socket.ts
@@ -20,7 +20,8 @@ type EventsWithCallback<T> = {
 
 type onceCallbackFirstArgument<Ev, E extends EventsMap> = Ev extends EventNames<E> ? (E[Ev] extends (...args: any) => void ? Parameters<E[Ev]>[0] : never) : never
 
-class CustomSocket<L extends EventsMap, E extends EventsMap> extends Socket<L, E> {
+export class CustomSocket<L extends EventsMap, E extends EventsMap> extends Socket<L, E> {
+  // @ts-ignore
   emitWithAck<Ev extends EventsWithCallback<E>>(timeout: number, ev: Ev, ...args: Head<EventParams<E, Ev>>): Promise<FirstParamType<Last<EventParams<E, Ev>>>> {
     return new Promise((resolve, reject) => {
       const timer = setTimeout(() => {
