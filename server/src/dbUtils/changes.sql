476f0b2b74e831b4b62ccd3402ac08bebd1e6d13
Oskar <oskar.baumann@baumann-automation.com>
2023-06-01 11:07:11 -0600

    Update database

--- server/src/dbUtils/changes.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE password_reset_requests (
-  token CHAR(64) NOT NULL PRIMARY KEY,
-  userid INT NOT NULL REFERENCES users (id) ON DELETE CASCADE,
-  time_of_request timestamptz NOT NULL DEFAULT current_timestamp,
-  valid BOOLEAN NOT NULL DEFAULT TRUE
-);
