204adbc6e7e97e4dcd691bae901b7cdf6b40eacb
Oskar Baumann <oskar.baumann@vectralis.com>
2023-12-28 13:08:31 +0100

    Debugging new Futuro model

--- server/src/bot/bots/Ruby.ts
+++ server/src/bot/bots/Ruby.ts
@@ -1,9 +1,9 @@
 // Hi, I'm ruby,
 // I am a RUle-Based sYstem, hoping to be a good AI
 
-import { ballStart, getPositionsBetweenStarts } from '../../game/ballUtils'
-import { MoveBall, MoveTextOrBall } from '../../sharedTypes/typesBall'
+import { MoveTextOrBall } from '../../sharedTypes/typesBall'
 import { AiData, AiInterface, getMovesFromCards } from '../simulation/simulation'
+import { ballInProximityOfHouse, normalizedNecessaryForwardMovesToEndOfGoal } from './utils'
 
 export class Ruby implements AiInterface {
   choose(data: AiData) {
@@ -14,7 +14,12 @@ export class Ruby implements AiInterface {
 }
 
 function getScoreFromMove(move: MoveTextOrBall, data: AiData): number {
-  return 1000 * ballIntoHouse(move) + 100 * ballInProximityOfHouse(move, data) + 10 * killedEnemy(move, data) + 1 * movesToHouse(move, data)
+  return (
+    1000 * ballIntoHouse(move) +
+    100 * (move.length === 4 ? ballInProximityOfHouse(move[3], move[2], data.balls) : 0) +
+    10 * killedEnemy(move, data) +
+    1 * (move.length === 4 ? normalizedNecessaryForwardMovesToEndOfGoal(move[3], move[2], data.balls) : 0)
+  )
 }
 
 function ballIntoHouse(move: MoveTextOrBall): number {
@@ -22,27 +27,9 @@ function ballIntoHouse(move: MoveTextOrBall): number {
   return move[3] >= 80 ? 1 : 0
 }
 
-function ballInProximityOfHouse(move: MoveTextOrBall, data: AiData): number {
-  if (move.length === 3) return 0
-  if (move[3] <= ballStart(move[2], data.balls) + 3 && move[3] > ballStart(move[2], data.balls)) return 1
-  if (necessaryMovesToHouse(move, data) < 13) return 1
-  return 0
-}
-
 function killedEnemy(move: MoveTextOrBall, data: AiData): number {
   if (move.length === 3) return 0
   const ballToKill = data.balls.find((ball) => ball.position === move[3])
   if (ballToKill == null) return 0
   return ballToKill.player % 2 !== data.gamePlayer % 2 ? 1 : -1
-}
-
-function movesToHouse(move: MoveTextOrBall, data: AiData): number {
-  if (move.length === 3) return 0
-  return 1 - necessaryMovesToHouse(move, data) / (data.nPlayers * getPositionsBetweenStarts(data.balls))
-}
-
-function necessaryMovesToHouse(move: MoveBall, data: AiData): number {
-  return ballStart(move[2], data.balls) > move[3]
-    ? ballStart(move[2], data.balls) - move[3] + 1
-    : ballStart(move[2], data.balls) + data.nPlayers * getPositionsBetweenStarts(data.balls) - move[3] + 1
 }
---
+++ server/src/bot/simulation/previewMove.ts
@@ -0,0 +1,67 @@
+import { MoveTextOrBall } from '../../sharedTypes/typesBall'
+import { AiData, getAiData } from './simulation'
+import { Game } from '../../game/game'
+import { initalizeStatistic } from '../../game/statistic'
+
+export function previewMove(data: AiData, move: MoveTextOrBall): AiData {
+  // Convert data back to game
+  const game = new Game(
+    data.nPlayers,
+    data.teams.length,
+    data.meisterVersion,
+    data.coop,
+    structuredClone({
+      nPlayers: data.nPlayers,
+      coop: data.coop,
+      teams: data.teams,
+
+      tradeDirection: data.tradeDirection,
+      aussetzenFlag: false,
+      teufelFlag: data.teufelFlag,
+      tradeFlag: false,
+      tradeCards: [],
+      narrFlag: Array.from({ length: data.nPlayers }, () => false),
+      activePlayer: data.activePlayer,
+      sevenChosenPlayer: data.sevenChosenPlayer,
+
+      balls: data.balls,
+      priorBalls: data.priorBalls,
+
+      gameEnded: false,
+      winningTeams: Array.from({ length: data.teams.length }, () => false),
+
+      cards: {
+        dealingPlayer: 0,
+        discardPlayer: 0,
+        discardedFlag: false,
+        deck: [],
+        discardPile: data.discardPile,
+        players: Array.from({ length: data.nPlayers }, (_, i) => (i === 0 ? data.cardsWithMoves.map((c) => c.title) : [])),
+        meisterVersion: data.meisterVersion,
+      },
+      cardsWithMoves: data.cardsWithMoves,
+
+      statistic: initalizeStatistic(data.nPlayers),
+      substitutedPlayerIndices: [],
+    })
+  )
+
+  // Perform move
+  if (!game.checkMove(move)) throw new Error('Invalid move')
+  game.performAction(move, 0)
+
+  // Reset activePlayer to 0
+  if (game.activePlayer !== 0) game.activePlayer = 0
+
+  // Calculate cards with moves
+  game.updateCardsWithMoves()
+  if (game.cardsWithMoves.length > 0 && game.cardsWithMoves.every((c) => !c.possible)) throw new Error('No possible moves found')
+
+  // convert back to data
+  return getAiData(game, 0, {
+    hadOneOrThirteen: data.hadOneOrThirteen,
+    tradedCards: Array.from({ length: data.nPlayers }, (_, i) => (i === 0 ? data.tradedCard : null)),
+    narrTradedCards: Array.from({ length: data.nPlayers }, (_, i) => (i === 0 ? data.narrTradedCards : null)),
+    previouslyUsedCards: data.previouslyUsedCards,
+  })
+}
