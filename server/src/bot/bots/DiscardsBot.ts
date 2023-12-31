4b70b9353f79609f08cf3f3881ebb360ad5049a6
Oskar Baumann <oskar.baumann@vectralis.com>
2023-12-31 16:50:11 +0100

    Add discardbot, implements #137

---
+++ server/src/bot/bots/DiscardsBot.ts
@@ -0,0 +1,21 @@
+import { CardType } from 'src/sharedTypes/typesCard'
+import { MoveTextOrBall } from '../../sharedTypes/typesBall'
+import { AiData } from '../simulation/output'
+import { getMovesFromCards } from '../simulation/simulation'
+
+// Most valuable first, if not in array then it is discarded first
+const orderedMostValuableCards = ['tac', 'trickser', 'teufel', '1', '13', '4']
+
+function getDiscardScore(card: CardType): number {
+  const index = orderedMostValuableCards.indexOf(card)
+  return index === -1 ? orderedMostValuableCards.length : index
+}
+
+export function discardBot(data: AiData): MoveTextOrBall | null {
+  const moves = getMovesFromCards(data.cardsWithMoves, data.gamePlayer)
+  if (moves.some((m) => m[2] != 'abwerfen' || m.length === 4)) {
+    return null
+  }
+
+  return moves.sort((a, b) => getDiscardScore(data.cardsWithMoves[b[1]].title) - getDiscardScore(data.cardsWithMoves[a[1]].title))[0]
+}
