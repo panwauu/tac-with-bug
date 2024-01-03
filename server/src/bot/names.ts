a66a921e1006984c904751223b61fd1ae108c929
Oskar Baumann <oskar.baumann@vectralis.com>
2024-01-03 21:35:27 +0100

    Fix tests and names

--- server/src/bot/names.ts
+++ server/src/bot/names.ts
@@ -1,5 +1,5 @@
-const names = ['KIara', 'Roberto', 'AIex', 'AIexa', 'AIberto', 'Roberta', 'Kiki', 'Kilian', 'Lisbot']
+const names = ['KIara', 'Roberto', 'AIex', 'Elisabot', 'AIberto', 'Roberta', 'Kiki', 'AIexa', 'Kilian', 'Lisbot', 'Bottina', 'Turk']
 
 export function getBotName(gameID: number, playerIndex: number) {
-  return `${names[(gameID + playerIndex * gameID) % names.length]}`
+  return `${names[(gameID + playerIndex * (gameID % 2)) % names.length]}`
 }
