67ac71632748908512a2b96d768d5dbed967cfe5
baumannos <oskar.baumann@baumann-automation.com>
2025-03-17 16:03:57 +0100

    Blocked by moderation message

---
+++ client/src/components/BlockedByModerationMessage.vue
@@ -0,0 +1,25 @@
+<template>
+  <Message
+    severity="warn"
+    :closable="false"
+    :pt="{
+      wrapper: {
+        style: 'padding: 0.75rem',
+      },
+      text: {
+        style: 'font-size: small',
+      },
+    }"
+    :title="$t('Moderation.you-are-blocked-explanation')"
+  >
+    {{ $t('Moderation.you-are-blocked-message', [new Date(blockedByModerationUntil).toLocaleDateString()]) }}
+  </Message>
+</template>
+
+<script setup lang="ts">
+import Message from 'primevue/message'
+
+defineProps<{ blockedByModerationUntil: string }>()
+</script>
+
+<style scoped></style>
