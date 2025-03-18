ab1e4d507de37902af90b33374f41e73a5e34b40
baumannos <oskar.baumann@baumann-automation.com>
2025-03-18 18:55:29 +0100

    Add table in prod

--- server/src/dbUtils/changes.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE moderation (
-  id SERIAL PRIMARY KEY,
-  insertedbyuserid INT NOT NULL,
-  userid INT,
-  email varchar(255),
-  blockeduntil timestamptz NOT NULL,
-  reason varchar(200)
-);
