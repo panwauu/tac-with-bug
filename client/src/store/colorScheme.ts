f8ed3a24fe6b1f67d18f0b34e41bb46d33b64f27
panwauu <baumann.oskar7@gmail.com>
2025-09-20 14:02:50 +0200

    Working color switcher

--- client/src/store/colorScheme.ts
+++ client/src/store/colorScheme.ts
@@ -1,17 +1,24 @@
 import { defineStore } from 'pinia'
 
+// Additional information:
+// To avoid flickering on initial load, there is an inline script in index.html that sets the class based on localstorage and system preference
+
 export type ColorScheme = 'light' | 'dark' | 'system'
 
-function applyColorScheme(scheme: ColorScheme) {
+function setColorSchemeInStorage(scheme: ColorScheme) {
+  if (scheme === 'light' || scheme === 'dark') {
+    localStorage.setItem('color-scheme', scheme)
+  } else {
+    localStorage.removeItem('color-scheme')
+  }
+}
+
+function applyColorSchemeClass(scheme: ColorScheme) {
   document.documentElement.classList.remove('tac-with-bug-dark')
 
   if (scheme === 'dark') {
     document.documentElement.classList.add('tac-with-bug-dark')
-    localStorage.setItem('color-scheme', 'dark')
-  } else if (scheme === 'light') {
-    localStorage.setItem('color-scheme', 'light')
-  } else {
-    localStorage.removeItem('color-scheme')
+  } else if (scheme === 'system') {
     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
       document.documentElement.classList.add('tac-with-bug-dark')
     }
@@ -19,9 +26,17 @@ function applyColorScheme(scheme: ColorScheme) {
 }
 
 export const useColorSchemeStore = defineStore('colorScheme', {
-  state: () => ({
-    colorScheme: (localStorage.getItem('color-scheme') as ColorScheme) || 'system',
-  }),
+  state: () => {
+    let stored = localStorage.getItem('color-scheme') as ColorScheme | null
+    let colorScheme: ColorScheme = 'system'
+
+    if (stored === 'light' || stored === 'dark') {
+      colorScheme = stored
+    }
+    applyColorSchemeClass(colorScheme)
+
+    return { colorScheme }
+  },
   getters: {
     isDark(state): boolean {
       if (state.colorScheme === 'dark') return true
@@ -41,7 +56,12 @@ export const useColorSchemeStore = defineStore('colorScheme', {
   actions: {
     setColorScheme(scheme: ColorScheme) {
       this.colorScheme = scheme
-      applyColorScheme(scheme)
+
+      // Persist only light/dark, remove for system
+      setColorSchemeInStorage(scheme)
+
+      // Apply css
+      applyColorSchemeClass(scheme)
     },
   },
 })
