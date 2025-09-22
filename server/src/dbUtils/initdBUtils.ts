754ea923ffde98607fb39288ef4f4ee010746067
panwauu <baumann.oskar7@gmail.com>
2025-09-22 13:50:24 +0200

    Improve

--- server/src/dbUtils/initdBUtils.ts
+++ server/src/dbUtils/initdBUtils.ts
@@ -15,18 +15,11 @@ export function initdBUtils(config?: pg.PoolConfig): pg.Pool {
     })
   } else if (process.env.NODE_ENV === 'development') {
     return new pg.Pool({
-      connectionString: 'postgres://postgres:postgres@localhost:5433/tac',
+      connectionString: process.env.DATABASE_URL ?? 'postgres://postgres:postgres@localhost:5433/tac',
     })
   } else if (process.env.NODE_ENV === 'test') {
     return new pg.Pool({
-      user: 'postgres',
-      host: 'localhost',
-      database: 'tac_test',
-      password: 'postgres',
-      port: 5433,
-      max: 15,
-      idleTimeoutMillis: 100,
-      connectionTimeoutMillis: 2000,
+      connectionString: process.env.DATABASE_URL ?? 'postgres://postgres:postgres@localhost:5433/tac_test',
     })
   }
 
@@ -34,8 +27,11 @@ export function initdBUtils(config?: pg.PoolConfig): pg.Pool {
 }
 
 export function initTestDatabaseClient(database: string): pg.Client {
+  const databaseUrl = process.env.DATABASE_URL ?? `postgres://postgres:postgres@localhost:5433/tac_test`
+  const newDatabaseUrl = databaseUrl.substring(0, databaseUrl.lastIndexOf('/') + 1) + database
+
   return new pg.Client({
-    connectionString: `postgresql://postgres:postgres@localhost:5433/${database}`,
+    connectionString: newDatabaseUrl,
     connectionTimeoutMillis: 2000,
   })
 }
--- server/src/test/setupTestEnvironment.ts
+++ server/src/test/setupTestEnvironment.ts
@@ -7,19 +7,15 @@ const chance = Chance()
 
 beforeAll(async () => {
   const databaseName = `test_temp_${chance.string({ length: 21, alpha: true, casing: 'lower' })}`
+  const databaseUrl = process.env.DATABASE_URL ?? `postgres://postgres:postgres@localhost:5433/tac_test`
+  const newDatabaseUrl = databaseUrl.substring(0, databaseUrl.lastIndexOf('/') + 1) + databaseName
+
   await prepareTestDatabase(databaseName)
   ;(global as any).databaseName = databaseName
   ;(global as any).testServer = new TacServer({
     serveApp: false,
     pgPoolConfig: {
-      user: 'postgres',
-      host: 'localhost',
-      database: databaseName,
-      password: 'postgres',
-      port: 5433,
-      max: 15,
-      idleTimeoutMillis: 100,
-      connectionTimeoutMillis: 2000,
+      connectionString: newDatabaseUrl,
     },
   })
   await (global as any).testServer.listen(0)
