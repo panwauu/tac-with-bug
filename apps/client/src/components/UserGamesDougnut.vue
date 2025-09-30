77e421eaa99c2452bb904f0020e5d13be9f59059
panwauu <baumann.oskar7@gmail.com>
2025-10-01 00:03:04 +0200

    Fix #212

--- apps/client/src/components/UserGamesDougnut.vue
+++ apps/client/src/components/UserGamesDougnut.vue
@@ -14,6 +14,7 @@ import type { GamesDistributionData } from '@/../../server/src/sharedTypes/types
 import { ref, onMounted, watch } from 'vue'
 import Chart from 'primevue/chart'
 import { useI18n } from 'vue-i18n'
+import { title } from 'process'
 
 const { t } = useI18n()
 const props = defineProps<{ data: GamesDistributionData }>()
@@ -42,8 +43,8 @@ const chartData = ref({
     },
     {
       data: [] as any[],
-      backgroundColor: ['#689F38', '#D32F2F', '#689F38', '#D32F2F', '#689F38', '#D32F2F', '#00000000'],
-      hoverBackgroundColor: ['#689F38', '#D32F2F', '#689F38', '#D32F2F', '#689F38', '#D32F2F', '#00000000'],
+      backgroundColor: ['#689F38', '#D32F2F', '#689F38', '#D32F2F', '#689F38', '#D32F2F', '#00000000', '#00000000'],
+      hoverBackgroundColor: ['#689F38', '#D32F2F', '#689F38', '#D32F2F', '#689F38', '#D32F2F', '#00000000', '#00000000'],
       labels: [
         `${t('Profile.DistributionGraph.4er')} ${t('Profile.DistributionGraph.won')}`,
         `${t('Profile.DistributionGraph.4er')} ${t('Profile.DistributionGraph.lost')}`,
@@ -51,6 +52,8 @@ const chartData = ref({
         `${t('Profile.DistributionGraph.6er')} ${t('Profile.DistributionGraph.lost')}`,
         t('Profile.DistributionGraph.teamEnded'),
         t('Profile.DistributionGraph.teamAborted'),
+        t('Profile.DistributionGraph.aborted'),
+        t('Profile.DistributionGraph.running'),
       ],
     },
   ],
@@ -66,12 +69,29 @@ const chartOptions = {
       },
     },
     tooltip: {
-      filter: function (context: any) {
-        return context.dataset.labels[context.dataIndex] != null
-      },
       callbacks: {
         label: function (context: any) {
           return `${context.dataset.labels[context.dataIndex]}: ${context.formattedValue}`
+        },
+        title: function (context: any) {
+          if (context == null) {
+            return ''
+          }
+
+          if (context[0].dataset.data.length === 7) {
+            let index = 0
+            if (context[0].dataIndex <= 1) {
+              index = 0
+            } else if (context[0].dataIndex <= 3) {
+              index = 1
+            } else if (context[0].dataIndex <= 5) {
+              index = 2
+            } else {
+              index = context[0].dataIndex - 3
+            }
+            return chartData.value.labels[index]
+          }
+          return context[0].dataset.labels[context[0].dataIndex]
         },
       },
     },
@@ -80,7 +100,7 @@ const chartOptions = {
 
 function resetGraph(data: GamesDistributionData) {
   chartData.value.datasets[0].data = [data.won4 + data.lost4, data.won6 + data.lost6, data.teamWon + data.teamAborted, data.aborted, data.running]
-  chartData.value.datasets[1].data = [data.won4, data.lost4, data.won6, data.lost6, data.teamWon, data.teamAborted, data.aborted + data.running]
+  chartData.value.datasets[1].data = [data.won4, data.lost4, data.won6, data.lost6, data.teamWon, data.teamAborted, data.aborted, data.running]
   userGamesDoughnutChart.value?.refresh()
 }
 
